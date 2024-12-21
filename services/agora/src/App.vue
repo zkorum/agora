8a3a4809f02be7eb7caed0be705e7b0d6df04481
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-12-20 21:12:08 -0500

    Switch to albert sans for the font

--- services/agora/src/App.vue
+++ services/agora/src/App.vue
@@ -19,15 +19,4 @@ onMounted(() => {
 });
 </script>
 
-<style lang="scss">
-body {
-  font-size: 1rem;
-  background-color: white;
-  color: rgb(24, 28, 31);
-}
-
-a {
-  color: rgb(24, 28, 31);
-  text-decoration: none;
-}
-</style>
+<style lang="scss"></style>
