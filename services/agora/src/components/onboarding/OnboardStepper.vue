1d083818f84494ee755a77e4b09661d04fb98ac0
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2024-12-24 17:49:09 -0500

    Switch progress bar to Qausar

--- services/agora/src/components/onboarding/OnboardStepper.vue
+++ services/agora/src/components/onboarding/OnboardStepper.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
-    <ProgressBar :value="value" :show-value="false" />
+    <q-linear-progress :value="value" />
   </div>
 </template>
 
 <script setup lang="ts">
-import ProgressBar from "primevue/progressbar";
-
 const props = defineProps<{
   currentStep: number;
   totalSteps: number;
 }>();
 
-const value = (props.currentStep / props.totalSteps) * 100;
+const value = props.currentStep / props.totalSteps;
 </script>
 
 <style scoped lang="scss"></style>
