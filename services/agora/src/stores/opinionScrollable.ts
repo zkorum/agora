245b8cc6764ec00c198a552bc00a01a2b398b048
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-02-14 16:07:29 -0500

    Fixed the opinion request bug by prioritizing the requested comment slug ID to the top of the display list

--- services/agora/src/stores/opinionScrollable.ts
+++ services/agora/src/stores/opinionScrollable.ts
@@ -1,6 +1,9 @@
-import { defineStore } from "pinia";
+import { defineStore, storeToRefs } from "pinia";
 import { OpinionItem } from "src/shared/types/zod";
+import { CommentFilterOptions } from "src/utils/component/opinion";
 import { ref } from "vue";
+import { useAuthenticationStore } from "./authentication";
+import { useNotify } from "src/utils/ui/notify";
 
 export const useOpinionScrollableStore = defineStore(
   "opinionScrollable",
@@ -9,6 +12,9 @@ export const useOpinionScrollableStore = defineStore(
     const opinionItemListPartial = ref<OpinionItem[]>([]);
 
     const hasMore = ref(true);
+
+    const { isAuthenticated } = storeToRefs(useAuthenticationStore());
+    const { showNotifyMessage } = useNotify();
 
     function loadMore() {
       const endSliceIndex = Math.min(
@@ -29,16 +35,76 @@ export const useOpinionScrollableStore = defineStore(
         opinionItemListPartial.value.concat(slicedList);
     }
 
-    function setupOpinionlist(opinionListFull: OpinionItem[]) {
+    function setupOpinionlist(
+      opinionListFull: OpinionItem[],
+      opinionSlugId: string
+    ) {
       opinionItemListPartial.value = [];
       opinionItemListFull.value = opinionListFull;
       hasMore.value = true;
+
+      if (opinionSlugId.length > 0) {
+        // Search for the specific opinion and move it to the front of list
+        let moveIndex = 0;
+        for (const [index, val] of opinionItemListFull.value.entries()) {
+          if (val.opinionSlugId == opinionSlugId) {
+            moveIndex = index;
+            break;
+          }
+        }
+
+        if (moveIndex > 0) {
+          opinionItemListFull.value.unshift(
+            opinionItemListFull.value.splice(moveIndex, 1)[0]
+          );
+        }
+      }
+
       loadMore();
+    }
+
+    function detectOpinionFilterBySlugId(
+      commentSlugId: string,
+      commentItemsNew: OpinionItem[],
+      commentItemsDiscover: OpinionItem[],
+      commentItemsModerated: OpinionItem[],
+      commentItemsHidden: OpinionItem[]
+    ): CommentFilterOptions | "not_found" {
+      for (const commentItem of commentItemsDiscover) {
+        if (commentItem.opinionSlugId == commentSlugId) {
+          return "discover";
+        }
+      }
+      for (const commentItem of commentItemsNew) {
+        if (commentItem.opinionSlugId == commentSlugId) {
+          return "new";
+        }
+      }
+
+      for (const commentItem of commentItemsModerated) {
+        if (commentItem.opinionSlugId == commentSlugId) {
+          return "moderated";
+        }
+      }
+
+      if (!isAuthenticated.value) {
+        showNotifyMessage("This opinion has been removed by the moderators");
+        return "discover";
+      } else {
+        for (const commentItem of commentItemsHidden) {
+          if (commentItem.opinionSlugId == commentSlugId) {
+            return "hidden";
+          }
+        }
+      }
+
+      return "not_found";
     }
 
     return {
       setupOpinionlist,
       loadMore,
+      detectOpinionFilterBySlugId,
       opinionItemListPartial,
       hasMore,
     };
