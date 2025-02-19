c2b9f05f82cd253cac774311c0cc96f4991f9728
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-02-18 22:04:52 -0500

    Add user profile picture to the side bar

--- services/agora/src/components/navigation/SideDrawer.vue
+++ services/agora/src/components/navigation/SideDrawer.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="container">
-    <div
-      v-for="settingItem in settingItemList"
-      :key="settingItem.name"
-      @click="enterRoute(settingItem.route, settingItem.requireAuth)"
-    >
-      <ZKHoverEffect :enable-hover="true">
-        <div class="settingItemStyle">
-          <q-icon :name="settingItem.icon" size="1rem" />
+    <div v-if="isAuthenticated" class="usernameBar">
+      <UserAvatar :user-name="profileData.userName" :size="35" />
+      <div>
+        {{ profileData.userName }}
+      </div>
+    </div>
 
-          {{ settingItem.name }}
-        </div>
-      </ZKHoverEffect>
+    <div class="menuListFlex">
+      <div
+        v-for="settingItem in settingItemList"
+        :key="settingItem.name"
+        @click="enterRoute(settingItem.route, settingItem.requireAuth)"
+      >
+        <ZKHoverEffect :enable-hover="true">
+          <div class="settingItemStyle">
+            <q-icon :name="settingItem.icon" size="1.5rem" />
+
+            {{ settingItem.name }}
+          </div>
+        </ZKHoverEffect>
+      </div>
     </div>
   </div>
 </template>
@@ -23,9 +32,12 @@ import { storeToRefs } from "pinia";
 import { useAuthenticationStore } from "src/stores/authentication";
 import { useDialog } from "src/utils/ui/dialog";
 import { useNavigationStore } from "src/stores/navigation";
+import UserAvatar from "../account/UserAvatar.vue";
+import { useUserStore } from "src/stores/user";
 
 const { isAuthenticated } = storeToRefs(useAuthenticationStore());
 const { showDrawer } = storeToRefs(useNavigationStore());
+const { profileData } = useUserStore();
 
 const { showLoginConfirmationDialog } = useDialog();
 
@@ -77,9 +89,26 @@ async function enterRoute(routeName: keyof RouteMap, requireAuth: boolean) {
   padding-top: 0.8rem;
   padding-bottom: 0.8rem;
   border-radius: 15px;
+  font-size: 1rem;
 }
 
 .settingItemStyle:hover {
   background-color: #f3f4f6;
+}
+
+.usernameBar {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  font-weight: 500;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  padding-left: 1rem;
+}
+
+.menuListFlex {
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
 }
 </style>
