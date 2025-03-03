30678cbe2455215e5400fdb04769e92938a3166f
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-03-03 18:59:39 -0500

    Move back button to the new set

--- services/agora/src/components/navigation/buttons/BackButton.vue
+++ services/agora/src/components/navigation/buttons/BackButton.vue
@@ -1,14 +1,12 @@
 <template>
-  <ZKButton
-    icon="mdi-arrow-left"
-    text-color="color-text-strong"
-    flat
-    @click="buttonClicked()"
-  />
+  <ZKButton text-color="color-text-strong" flat @click="buttonClicked()">
+    <ZKIcon name="ci:chevron-left" size="1.2rem" color="7D7A85" />
+  </ZKButton>
 </template>
 
 <script setup lang="ts">
 import ZKButton from "src/components/ui-library/ZKButton.vue";
+import ZKIcon from "src/components/ui-library/ZKIcon.vue";
 import { useGoBackButtonHandler } from "src/utils/nav/goBackButton";
 
 const goBackButtonHandler = useGoBackButtonHandler();
