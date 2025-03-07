6906a46b3e95a38d869f9885233eaf5d82e835c7
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-03-06 23:59:04 -0500

    Auto size the middle section of the default menu bar

--- services/agora/src/components/navigation/header/DefaultMenuBar.vue
+++ services/agora/src/components/navigation/header/DefaultMenuBar.vue
@@ -107,7 +107,7 @@ function scrollToTop() {
 <style scoped lang="scss">
 .gridContainer {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr auto 1fr;
   grid-template-rows: 1fr;
   gap: 1rem 1rem;
   grid-template-areas: ". . .";
