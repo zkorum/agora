7054ca14e33e6f66e3b3c9e471f0f8463b52477a
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-03-18 07:06:47 -0400

    Complete the user intention loop for user opinions

--- services/agora/src/components/ui-library/ZKEditor.vue
+++ services/agora/src/components/ui-library/ZKEditor.vue
@@ -8,7 +8,7 @@
     >
       <q-editor
         ref="editorRef"
-        v-model="commentText"
+        v-model="modelText"
         :class="{
           whiteBackground: addBackgroundColor,
           plainBackground: !addBackgroundColor,
@@ -25,9 +25,9 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from "vue";
+import { ref } from "vue";
 
-const modelText = defineModel<string>();
+const modelText = defineModel<string>({ required: true });
 
 defineProps<{
   showToolbar: boolean;
@@ -41,40 +41,14 @@ const emit = defineEmits(["manuallyFocused"]);
 
 const editorRef = ref<HTMLElement | null>(null);
 
-const commentText = ref("");
-
 const toolbarButtons = [
   ["bold", "italic", "strike", "underline"],
   ["undo", "redo"],
 ];
 
-onMounted(() => {
-  // processFocus();
-});
-
-/*
-watch(() => props.focusEditor, () => {
-  if (props.focusEditor) {
-    processFocus();
-  }
-})
-  */
-
-watch(commentText, () => {
-  modelText.value = commentText.value;
-});
-
 function editorFocused() {
   emit("manuallyFocused");
 }
-
-/*
-function processFocus() {
-  if (props.focusEditor == true) {
-    editorRef.value?.focus();
-  }
-}
-*/
 
 function onPaste(evt: Event) {
   // Let inputs do their thing, so we don't break pasting of links.
