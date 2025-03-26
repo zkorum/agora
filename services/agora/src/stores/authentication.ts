8f15d2e25472eeeae47f79a0c15d53ba7ca07da1
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-03-26 15:01:49 -0400

    Switch authentication state back to memory

--- services/agora/src/stores/authentication.ts
+++ services/agora/src/stores/authentication.ts
@@ -1,11 +1,10 @@
-import { useStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useAuthenticationStore = defineStore("authentication", () => {
   const verificationPhoneNumber = ref("");
   const verificationDefaultCallingCode = ref("");
-  const isAuthenticated = useStorage("isAuthenticated", false);
+  const isAuthenticated = ref(false);
 
   return {
     isAuthenticated,
