208810cef5da0178ffb1537b9b72a094683bc25a
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-04-09 11:17:49 -0400

    Apply fixes for compilation and db migration

--- services/api/database/flyway/V0005__acoustic_the_hunter.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "conversation" ADD COLUMN "is_indexed" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "conversation" ADD COLUMN "is_login_required" boolean DEFAULT false NOT NULL;
