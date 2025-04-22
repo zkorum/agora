2ffee0d7017d1428a576d8e9d4ab4137eb7fcfa8
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2025-04-22 08:15:16 +0200

    fix(db): adapt drizzle to modified V0005

--- services/api/drizzle/0005_lucky_barracuda.sql
+++ services/api/drizzle/0005_lucky_barracuda.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "conversation" ADD COLUMN "organization_id" integer;--> statement-breakpoint
 ALTER TABLE "conversation" ADD COLUMN "index_conversation_at" timestamp (0);--> statement-breakpoint
-ALTER TABLE "conversation" ADD COLUMN "is_indexed" boolean NOT NULL;--> statement-breakpoint
-ALTER TABLE "conversation" ADD COLUMN "is_login_required" boolean NOT NULL;--> statement-breakpoint
+ALTER TABLE "conversation" ADD COLUMN "is_indexed" boolean NOT NULL DEFAULT true;--> statement-breakpoint
+ALTER TABLE "conversation" ADD COLUMN "is_login_required" boolean NOT NULL DEFAULT true;--> statement-breakpoint
 ALTER TABLE "conversation" ADD CONSTRAINT "conversation_organization_id_organization_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("id") ON DELETE no action ON UPDATE no action;
