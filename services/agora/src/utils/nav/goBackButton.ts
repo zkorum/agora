ac36ffcd7c9fe70e12108d6563a75e0eded9d695
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-05-01 07:47:05 -0400

    Revise back button to use save navigation

--- services/agora/src/utils/nav/goBackButton.ts
+++ services/agora/src/utils/nav/goBackButton.ts
@@ -3,9 +3,17 @@ import { useRouter } from "vue-router";
 export function useGoBackButtonHandler() {
   const router = useRouter();
 
-  async function goBack() {
-    router.go(-1);
+  async function safeNavigateBack() {
+    const currentIndex = window.history.length - 1;
+
+    if (currentIndex > 1) {
+      router.go(-1);
+    } else {
+      await router.replace({ name: "/" });
+    }
   }
 
-  return { goBack };
+  return {
+    safeNavigateBack,
+  };
 }
