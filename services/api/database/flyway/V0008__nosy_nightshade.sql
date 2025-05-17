f35fbe88eafd7f20da9f7f26ad14cd3a4c63a934
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2025-05-17 19:46:55 +0200

    fix(clusters): migration script to avoid A/B

---
+++ services/api/database/flyway/V0008__nosy_nightshade.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "polis_cluster" ALTER COLUMN "ai_label" SET DATA TYPE varchar(100);--> statement-breakpoint
+ALTER TABLE "polis_cluster" ALTER COLUMN "ai_summary" SET DATA TYPE varchar(1000);
