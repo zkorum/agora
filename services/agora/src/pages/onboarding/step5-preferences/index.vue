6167375b8640a1909f3b1d041f7fbb32a1db06be
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-05-16 23:57:04 -0400

    Migrate preference page to standard onboarding layout

--- services/agora/src/pages/onboarding/step5-preferences/index.vue
+++ services/agora/src/pages/onboarding/step5-preferences/index.vue
@@ -1,107 +1,70 @@
 <template>
-  <DrawerLayout
-    :general-props="{
-      addGeneralPadding: false,
-      addBottomPadding: false,
-      enableHeader: true,
-      enableFooter: false,
-      reducedWidth: true,
-    }"
-  >
-    <StepperLayout
-      :submit-call-back="() => {}"
-      :current-step="6"
-      :total-steps="6"
-      :enable-next-button="true"
-      :show-next-button="false"
-      :show-loading-button="false"
-    >
-      <template #header>
-        <InfoHeader
-          title="Content Preferences"
-          description=""
-          icon-name="mdi-star-box"
-        />
-      </template>
+  <OnboardingLayout>
+    <template #body><DefaultImageExample /> </template>
 
-      <template #body>
-        <!--
-        <div class="headerContainer">
-          <q-icon name="mdi-translate" size="2rem" />
-          What languages do you speak?
-        </div>
-
-        <div class="tagContainer">
-          <div v-for="lang in languageList" :key="lang" class="tagWrapper">
-            <q-btn
-              outline
-              no-caps
-              class="tagStyle"
-              rounded
-              size="small"
-              text-color="primary"
-            >
-              <div class="tagFlexContainer">
-                <div>
-                  {{ lang }}
-                </div>
-                <q-icon name="mdi-close" />
-              </div>
-            </q-btn>
-          </div>
-          <q-btn round outline no-caps color="secondary" icon="mdi-plus" />
-        </div>
-        -->
-
-        <div class="headerContainer">
-          <q-icon name="mdi-pound" size="2rem" />
-          <span class="title"> Select topics to get started </span>
-        </div>
-
-        <div class="tagContainer">
-          <Button
-            v-for="topic in fullTopicList"
-            :key="topic.code"
-            :label="topic.name"
-            rounded
-            :icon="followedTopicCodeSet.has(topic.code) ? 'pi pi-check' : ''"
-            :pt="
-              followedTopicCodeSet.has(topic.code)
-                ? {
-                    root: {
-                      class: 'followingButtonStyle generalStyle',
-                    },
-                  }
-                : {
-                    root: {
-                      class: 'followButtonStyle generalStyle',
-                    },
-                  }
-            "
+    <template #footer>
+      <StepperLayout
+        :submit-call-back="() => {}"
+        :current-step="4"
+        :total-steps="5"
+        :enable-next-button="false"
+        :show-next-button="false"
+        :show-loading-button="false"
+      >
+        <template #header>
+          <InfoHeader
+            title="Select topics to get started"
+            description=""
+            icon-name="mdi-pound"
           />
-        </div>
+        </template>
 
-        <ZKButton
-          button-type="largeButton"
-          label="Save & Close"
-          color="primary"
-          @click="clickedSaveAndClose()"
-        />
-      </template>
-    </StepperLayout>
-  </DrawerLayout>
+        <template #body>
+          <div class="tagContainer">
+            <Button
+              v-for="topic in fullTopicList"
+              :key="topic.code"
+              :label="topic.name"
+              rounded
+              :icon="followedTopicCodeSet.has(topic.code) ? 'pi pi-check' : ''"
+              :pt="
+                followedTopicCodeSet.has(topic.code)
+                  ? {
+                      root: {
+                        class: 'followingButtonStyle generalStyle',
+                      },
+                    }
+                  : {
+                      root: {
+                        class: 'followButtonStyle generalStyle',
+                      },
+                    }
+              "
+            />
+          </div>
+
+          <ZKButton
+            button-type="largeButton"
+            label="Save & Close"
+            color="primary"
+            @click="clickedSaveAndClose()"
+          />
+        </template>
+      </StepperLayout>
+    </template>
+  </OnboardingLayout>
 </template>
 
 <script setup lang="ts">
 import StepperLayout from "src/components/onboarding/StepperLayout.vue";
 import InfoHeader from "src/components/onboarding/InfoHeader.vue";
 import ZKButton from "src/components/ui-library/ZKButton.vue";
-import DrawerLayout from "src/layouts/DrawerLayout.vue";
 import { storeToRefs } from "pinia";
 import { useTopicStore } from "src/stores/topic";
 import { onMounted } from "vue";
 import Button from "primevue/button";
 import { useLoginIntentionStore } from "src/stores/loginIntention";
+import OnboardingLayout from "src/layouts/OnboardingLayout.vue";
 
 const { loadTopicsData } = useTopicStore();
 const { fullTopicList, followedTopicCodeSet } = storeToRefs(useTopicStore());
@@ -125,22 +88,6 @@ async function clickedSaveAndClose() {
   gap: 1rem;
 }
 
-.headerContainer {
-  display: flex;
-  gap: 1rem;
-  align-items: center;
-}
-
-.tagWrapper {
-  position: relative;
-}
-
-.tagFlexContainer {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-}
-
 .generalStyle {
   font-weight: 400;
   font-size: 1rem;
@@ -160,12 +107,8 @@ async function clickedSaveAndClose() {
 }
 
 .followingButtonStyle {
-  background-color: $primary;
+  background-color: #a6a3d6;
   border-color: transparent;
   color: white;
-}
-
-.title {
-  font-size: 1.1rem;
 }
 </style>
