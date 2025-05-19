92094e9269e7540c9c9f820c5ed0a360e4d3a3e0
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2025-05-19 21:38:19 +0200

    feat(analysis): better algorithms

---
+++ services/api/src/utils/dataStructure.ts
@@ -0,0 +1,27 @@
+export function createInterleavingMapFrom<K, V>(
+    map1: Map<K, V>,
+    map2: Map<K, V>,
+): Map<K, V> {
+    const interleavedMap = new Map<K, V>();
+    const map1Entries = Array.from(map1.entries());
+    const map2Entries = Array.from(map2.entries());
+    for (let i = 0; i < Math.max(map1.size, map2.size); i++) {
+        if (i < map1.size) {
+            const key = map1Entries[i][0];
+            const value = map1Entries[i][1];
+            // avoiding duplicates TODO: we actually don't need Map anymore... switch to a Set?
+            if (!interleavedMap.has(key)) {
+                interleavedMap.set(key, value);
+            }
+        }
+        if (i < map2.size) {
+            const key = map2Entries[i][0];
+            const value = map2Entries[i][1];
+            // avoiding duplicates TODO: we actually don't need Map anymore... switch to a Set?
+            if (!interleavedMap.has(key)) {
+                interleavedMap.set(key, value);
+            }
+        }
+    }
+    return interleavedMap;
+}
