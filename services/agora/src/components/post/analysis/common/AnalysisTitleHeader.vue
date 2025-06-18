93614b4cf5f1c50ae38f24acea8586e3c2ac851b
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-06-18 05:31:02 -0400

    Improve the button style for each section

--- services/agora/src/components/post/analysis/common/AnalysisTitleHeader.vue
+++ services/agora/src/components/post/analysis/common/AnalysisTitleHeader.vue
@@ -10,9 +10,23 @@
         </div>
       </div>
 
-      <div v-if="showChoice == 'infoButton'">Info button</div>
-      <div v-if="showChoice == 'viewMore'">View more</div>
-      <div v-if="showChoice == 'whatIsThis'">What is this</div>
+      <button
+        v-if="showChoice == 'infoButton'"
+        class="choice-button"
+        @click.stop
+      >
+        <i class="pi pi-info-circle" :style="{ color: '#6D6A74' }"></i>
+      </button>
+      <button v-if="showChoice == 'viewMore'" class="choice-button" @click.stop>
+        View more
+      </button>
+      <button
+        v-if="showChoice == 'whatIsThis'"
+        class="choice-button"
+        @click.stop
+      >
+        What is this
+      </button>
     </div>
   </div>
 </template>
@@ -40,5 +54,25 @@ const props = defineProps<{
   display: flex;
   gap: 1rem;
   align-items: center;
+}
+
+.choice-button {
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  color: #6d6a74;
+  border-radius: 4px;
+  transition: background-color 0.2s;
+  white-space: nowrap;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.05);
+  }
 }
 </style>
