bfdb08c3292029bad4c434feff56cd9e564978ab
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-06-19 14:23:31 -0400

    Unify all sections with a common one

---
+++ services/agora/src/components/post/analysis/common/AnalysisTabBase.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <AnalysisSectionWrapper>
+      <template #header>
+        <AnalysisTitleHeader
+          :show-star-in-title="showStarInTitle"
+          :title="title"
+          :show-choice="showChoice"
+        />
+      </template>
+
+      <template #body>
+        <ConsensusItem
+          v-for="consensusItem in itemList"
+          :key="consensusItem.description"
+          :consensus-item="consensusItem"
+        />
+      </template>
+    </AnalysisSectionWrapper>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AnalysisSectionWrapper from "./AnalysisSectionWrapper.vue";
+import AnalysisTitleHeader from "./AnalysisTitleHeader.vue";
+import ConsensusItem from "../consensusTab/ConsensusItem.vue";
+import { ConsensusItemData } from "src/utils/component/analysis/analysisTypes";
+
+defineProps<{
+  title: string;
+  itemList: ConsensusItemData[];
+  showChoice: "infoButton" | "viewMore" | "whatIsThis";
+  showStarInTitle: boolean;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
