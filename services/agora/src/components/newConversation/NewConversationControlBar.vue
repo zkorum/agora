69bca9450b8cf33bc4aff9abeb61be7856aabbcd
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-06-27 16:11:44 -0400

    Add missing file

---
+++ services/agora/src/components/newConversation/NewConversationControlBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="control-bar">
+    <UserAvatar
+      :key="profileData.userName"
+      :user-identity="profileData.userName"
+      :size="35"
+    />
+
+    <div @click="showAsDialog()">
+      <FollowButton
+        :label="'As ABC'"
+        :variant="''"
+        :is-following="true"
+        :icon="'pi pi-chevron-down'"
+      />
+    </div>
+
+    <div @click="showPublicDialog()">
+      <FollowButton
+        :label="'Public'"
+        :variant="''"
+        :is-following="true"
+        :icon="'pi pi-chevron-down'"
+      />
+    </div>
+
+    <FollowButton
+      :label="'Add poll'"
+      :variant="''"
+      :is-following="true"
+      :icon="'pi pi-plus'"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useUserStore } from "src/stores/user";
+import UserAvatar from "src/components/account/UserAvatar.vue";
+import FollowButton from "src/components/ui-library/buttons/FollowButton.vue";
+
+const emit = defineEmits<{
+  showAsDialog: [];
+  showPublicDialog: [];
+}>();
+
+const { profileData } = storeToRefs(useUserStore());
+
+const showAsDialog = () => {
+  emit("showAsDialog");
+};
+
+const showPublicDialog = () => {
+  emit("showPublicDialog");
+};
+</script>
+
+<style scoped lang="scss">
+.control-bar {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+</style>
