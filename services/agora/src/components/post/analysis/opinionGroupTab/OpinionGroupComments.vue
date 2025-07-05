48d176cd4c588089beb70587777874067495083a
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2025-07-05 02:08:17 +0200

    feat(polis): add "all other groups" as well

--- services/agora/src/components/post/analysis/opinionGroupTab/OpinionGroupComments.vue
+++ services/agora/src/components/post/analysis/opinionGroupTab/OpinionGroupComments.vue
@@ -58,7 +58,9 @@ const props = defineProps<{
   polis: ExtendedConversationPolis;
 }>();
 
-const displayMode = ref<"current" | "others">("current");
+const displayMode = ref<"current" | "all_other_groups" | "all_others">(
+  "current"
+);
 
 watch(
   () => props.currentClusterTab,
@@ -71,42 +73,83 @@ function getActiveVotes(comment: OpinionItem) {
   const currentClusterStats = comment.clustersStats.find(
     (cv) => cv.key === props.currentClusterTab
   );
-  if (displayMode.value === "current") {
-    return (
-      currentClusterStats || {
-        numAgrees: 0,
-        numDisagrees: 0,
-        // numPass: 0,
-        numUsers: 0,
-      }
-    );
-  } else {
-    return {
-      numAgrees:
-        comment.numAgrees -
-        (currentClusterStats !== undefined ? currentClusterStats.numAgrees : 0),
-      numDisagrees:
-        comment.numDisagrees -
-        (currentClusterStats !== undefined
-          ? currentClusterStats.numDisagrees
-          : 0),
-      numUsers:
-        comment.numParticipants -
-        (currentClusterStats !== undefined ? currentClusterStats.numUsers : 0),
-    };
+  const allOthersClustersStats = comment.clustersStats.filter(
+    (clusterStats) => clusterStats.key !== props.currentClusterTab
+  );
+  switch (displayMode.value) {
+    case "current":
+      return (
+        currentClusterStats || {
+          numAgrees: 0,
+          numDisagrees: 0,
+          // numPass: 0,
+          numUsers: 0,
+        }
+      );
+    case "all_other_groups":
+      return {
+        numAgrees: Object.values(allOthersClustersStats).reduce(
+          (sum, clusterStats) => sum + clusterStats.numAgrees,
+          0
+        ),
+        numDisagrees: Object.values(allOthersClustersStats).reduce(
+          (sum, clusterStats) => sum + clusterStats.numDisagrees,
+          0
+        ),
+        numUsers: Object.values(allOthersClustersStats).reduce(
+          (sum, clusterStats) => sum + clusterStats.numUsers,
+          0
+        ),
+      };
+    case "all_others":
+      return {
+        numAgrees:
+          comment.numAgrees -
+          (currentClusterStats !== undefined
+            ? currentClusterStats.numAgrees
+            : 0),
+        numDisagrees:
+          comment.numDisagrees -
+          (currentClusterStats !== undefined
+            ? currentClusterStats.numDisagrees
+            : 0),
+        numUsers:
+          comment.numParticipants -
+          (currentClusterStats !== undefined
+            ? currentClusterStats.numUsers
+            : 0),
+      };
   }
 }
 
 const currentModeName = computed(() => {
-  return displayMode.value === "current" ? "This group" : "All others";
+  return displayMode.value === "current"
+    ? "This group"
+    : displayMode.value === "all_others"
+      ? "All others"
+      : "All other groups";
 });
 
-const toggleMode = () => {
-  displayMode.value = displayMode.value === "current" ? "others" : "current";
+const toggleNextMode = () => {
+  displayMode.value =
+    displayMode.value === "current"
+      ? "all_other_groups"
+      : displayMode.value === "all_other_groups"
+        ? "all_others"
+        : "current";
 };
 
-const navigateToPreviousMode = toggleMode;
-const navigateToNextMode = toggleMode;
+const togglePreviousMode = () => {
+  displayMode.value =
+    displayMode.value === "current"
+      ? "all_others"
+      : displayMode.value === "all_other_groups"
+        ? "current"
+        : "all_other_groups";
+};
+
+const navigateToPreviousMode = togglePreviousMode;
+const navigateToNextMode = toggleNextMode;
 </script>
 
 <style lang="scss" scoped>
