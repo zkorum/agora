741f337c080534a29426ebe2bc84fa5be959c07c
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2025-07-11 00:44:12 +0200

    fix: generate new sql script (#272)

---
+++ services/api/drizzle/0012_robust_blockbuster.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "opinion" ALTER COLUMN "polis_ga_consensus_pa" SET DEFAULT 0;--> statement-breakpoint
+ALTER TABLE "opinion" ALTER COLUMN "polis_ga_consensus_pa" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "opinion" ALTER COLUMN "polis_priority" SET DEFAULT 0;--> statement-breakpoint
+ALTER TABLE "opinion" ALTER COLUMN "polis_priority" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "opinion" ALTER COLUMN "polis_divisiveness" SET DEFAULT 0;--> statement-breakpoint
+ALTER TABLE "opinion" ALTER COLUMN "polis_divisiveness" SET NOT NULL;
