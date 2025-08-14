5aefdb6ff8747b1243cd98305691f2a815507c32
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-08-14 18:22:57 -0400

    Update backend to support display language

--- services/api/drizzle/0021_sticky_green_goblin.sql
+++ services/api/drizzle/0021_sticky_green_goblin.sql
@@ -1,3 +1,13 @@
+CREATE TABLE "user_display_language" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" uuid NOT NULL,
+	"language_code" varchar(35) NOT NULL,
+	"is_deleted" boolean DEFAULT false NOT NULL,
+	"deleted_at" timestamp (0),
+	"created_at" timestamp (0) DEFAULT now() NOT NULL,
+	CONSTRAINT "user_display_language_unique" UNIQUE("user_id","language_code")
+);
+--> statement-breakpoint
 CREATE TABLE "user_spoken_languages" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"user_id" uuid NOT NULL,
@@ -10,5 +20,7 @@ CREATE TABLE "user_spoken_languages" (
 --> statement-breakpoint
 DROP TABLE "user_language_preference" CASCADE;--> statement-breakpoint
 DROP TABLE "user_language" CASCADE;--> statement-breakpoint
+ALTER TABLE "user_display_language" ADD CONSTRAINT "user_display_language_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_spoken_languages" ADD CONSTRAINT "user_spoken_languages_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "user_display_language_user_idx" ON "user_display_language" USING btree ("user_id");--> statement-breakpoint
 CREATE INDEX "user_spoken_languages_user_idx" ON "user_spoken_languages" USING btree ("user_id");
