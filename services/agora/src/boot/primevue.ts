c9b632f05ca27f8d6270ea82fa6b66d1d22510ca
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-08-16 01:45:07 -0400

    Improve the onbaording dialog's close system

--- services/agora/src/boot/primevue.ts
+++ services/agora/src/boot/primevue.ts
@@ -6,6 +6,9 @@ import Button from "primevue/button";
 import Chip from "primevue/chip";
 import DatePicker from "primevue/datepicker";
 import InputOtp from "primevue/inputotp";
+import InputText from "primevue/inputtext";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
 
 export default defineBoot(({ app }) => {
   app.use(PrimeVue, {
@@ -25,4 +28,7 @@ export default defineBoot(({ app }) => {
   app.component("PrimeChip", Chip);
   app.component("PrimeDatePicker", DatePicker);
   app.component("PrimeInputOtp", InputOtp);
+  app.component("PrimeInputText", InputText);
+  app.component("PrimeIconField", IconField);
+  app.component("PrimeInputIcon", InputIcon);
 });
