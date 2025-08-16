0de75be8bfbc246fa9e46a7a7628de69d21ad0f3
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-08-16 04:29:39 -0400

    Unify the button styles

--- services/agora/src/boot/primevue.ts
+++ services/agora/src/boot/primevue.ts
@@ -1,5 +1,6 @@
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
+import { definePreset } from "@primeuix/themes";
 import { defineBoot } from "#q-app/wrappers";
 
 import Button from "primevue/button";
@@ -10,10 +11,37 @@ import InputText from "primevue/inputtext";
 import IconField from "primevue/iconfield";
 import InputIcon from "primevue/inputicon";
 
+const customPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: "#f3f1ff",
+      100: "#e9e5ff",
+      200: "#d6ccff",
+      300: "#baa7ff",
+      400: "#9a75ff",
+      500: "#6b4eff",
+      600: "#5d42e6",
+      700: "#4f35cc",
+      800: "#422cb3",
+      900: "#382899",
+      950: "#241a66",
+    },
+  },
+  primitive: {
+    borderRadius: {
+      xs: "15px",
+      sm: "15px",
+      md: "15px",
+      lg: "15px",
+      xl: "15px",
+    },
+  },
+});
+
 export default defineBoot(({ app }) => {
   app.use(PrimeVue, {
     theme: {
-      preset: Aura,
+      preset: customPreset,
       options: {
         darkModeSelector: "light",
         cssLayer: false,
