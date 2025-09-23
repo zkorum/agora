43c39a376edcfcae2dc14f63b8bd1eed22cf086a
Nicolas Gimenez <46368291+nicobao@users.noreply.github.com>
2025-09-23 12:13:35 +0200

    chore: sentry

--- services/agora/src/boot/sentry.ts
+++ services/agora/src/boot/sentry.ts
@@ -8,6 +8,6 @@ export default defineBoot(({ app }) => {
     dsn: "https://045c836ef8e742f3696524dd09ccdb01@o4510068006780928.ingest.de.sentry.io/4510068008484944",
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
-    sendDefaultPii: true,
+    sendDefaultPii: false,
   });
 });
