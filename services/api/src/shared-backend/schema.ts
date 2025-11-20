d73189959767e8fa58e33de70e5c9ef115f7a182
Edmund Wong <9814980+e32wong@users.noreply.github.com>
2025-11-19 21:54:22 -0500

    Add unique constraint to the notification ID in the schema

--- services/api/src/shared-backend/schema.ts
+++ services/api/src/shared-backend/schema.ts
@@ -1685,7 +1685,8 @@ export const notificationOpinionVoteTable = pgTable(
         id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
         notificationId: integer("notification_id")
             .references(() => notificationTable.id)
-            .notNull(),
+            .notNull()
+            .unique(),
         opinionId: integer("opinion_id")
             .references(() => opinionTable.id)
             .notNull(),
@@ -1706,7 +1707,8 @@ export const notificationNewOpinionTable = pgTable("notification_new_opinion", {
     id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
     notificationId: integer("notification_id")
         .references(() => notificationTable.id)
-        .notNull(),
+        .notNull()
+        .unique(),
     authorId: uuid("author_id")
         .references(() => userTable.id)
         .notNull(),
@@ -1728,7 +1730,8 @@ export const notificationExportTable = pgTable("notification_export", {
     id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
     notificationId: integer("notification_id")
         .references(() => notificationTable.id)
-        .notNull(),
+        .notNull()
+        .unique(),
     exportId: integer("export_id")
         .references(() => conversationExportTable.id)
         .notNull(),
